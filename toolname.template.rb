0361d91ef1eaa7c10d33c5fab8cd372fb0b36d32
Akshat Patel <akshat.patel16@gmail.com>
2024-12-27 16:36:45 +0530

    Add template

---
+++ toolname.template.rb
@@ -0,0 +1,16 @@
+class <toolname> < Formula
+  desc "<toolname> CLI Tool"
+  homepage "https://github.com/PreternaturalAI/homebrew-preternatural"
+  url
+  sha256
+  version
+
+  def install
+    system "unzip", "-o", cached_download
+    bin.install "<toolname>/bin/<toolname>"
+  end
+
+  test do
+    system "#{bin}/<toolname>"
+  end
+end
